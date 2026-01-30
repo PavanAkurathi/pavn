e572732a4680dce222326e872d177ee8f410f8a7
pavanAkurathi <avpavan@gmail.com>
2026-01-30 10:09:05 -0500

    feat: Upgrade turbo, add Trigger.dev dependency, and refactor job configuration to TypeScript.

--- packages/jobs/trigger.config.ts
+++ packages/jobs/trigger.config.ts
@@ -1,15 +1,18 @@
-{
-  "project": "proj_workershive",
-  "triggerDirectories": ["packages/jobs/src"],
-  "triggerUrl": "https://api.trigger.dev",
-  "retries": {
-    "enabledInDev": false,
-    "default": {
-      "maxAttempts": 3,
-      "minTimeoutInMs": 1000,
-      "maxTimeoutInMs": 10000,
-      "factor": 2,
-      "randomize": true
+import type { TriggerConfig } from "@trigger.dev/sdk/v3";
+
+export const config: TriggerConfig = {
+  project: "proj_workershive",
+  triggerDirectories: ["packages/jobs/src"],
+  maxDuration: 300,
+
+  retries: {
+    enabledInDev: false,
+    default: {
+      maxAttempts: 3,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 10000,
+      factor: 2,
+      randomize: true
     }
   }
-}
+};
