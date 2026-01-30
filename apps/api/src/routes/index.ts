2a06925817eeed12c0ff3267c1a54dc052f0f62f
PavanAkurathi <avpavan@gmail.com>
2026-01-29 23:09:20 -0500

    refactor(api): add routes index exports

---
+++ apps/api/src/routes/index.ts
@@ -0,0 +1,9 @@
+// apps/api/src/routes/index.ts
+// Route exports
+
+export { shiftsRouter } from "./shifts";
+export { workerRouter } from "./worker";
+export { timesheetsRouter } from "./timesheets";
+export { billingRouter } from "./billing";
+export { organizationsRouter } from "./organizations";
+export { geofenceRouter } from "./geofence";
