c4c131fee99c148b59a0477298645f5652053136
pavanAkurathi <avpavan@gmail.com>
2026-02-12 23:23:10 -0500

    fix(shifts): export missing modules including locations, workers, and reporting

--- packages/shifts/src/index.ts
+++ packages/shifts/src/index.ts
@@ -8,22 +8,35 @@ export * from "./modules/shifts/upcoming";
 export * from "./modules/shifts/history";
 export * from "./modules/shifts/get-by-id";
 export * from "./modules/shifts/cancel";
+export * from "./modules/shifts/delete-drafts";
+export * from "./modules/shifts/get-shift-group";
+export * from "./modules/shifts/pending";
 
 export * from "./modules/time-tracking/approve";
 export * from "./modules/time-tracking/assign";
 export * from "./modules/time-tracking/service"; // Was assignments.ts
 export * from "./modules/time-tracking/worker-shifts";
+export * from "./modules/time-tracking/get-timesheets";
+export * from "./modules/time-tracking/overlap";
+export * from "./modules/time-tracking/update-timesheet";
 
 export * from "./modules/workers/invite-worker";
 export * from "./modules/workers/get-crew";
 export * from "./modules/workers/get-availability";
 export * from "./modules/workers/set-availability";
+export * from "./modules/workers/deactivate-worker";
+export * from "./modules/workers/reactivate-worker";
+export * from "./modules/workers/update-worker";
 
 export * from "./modules/locations/get-locations";
 export * from "./modules/locations/create-location";
 export * from "./modules/locations/update-location";
+export * from "./modules/locations/delete-location";
+export * from "./modules/locations/geocoding";
 
 export * from "./modules/reporting/export-timesheets";
 export * from "./modules/reporting/get-timesheets-report";
+export * from "./modules/reporting/get-report-filters";
 
 export * from "./modules/org/get-settings";
+export * from "./modules/org/update-settings";
